f41850d51580712b693fefe5b61d230f254ced60
gaaats <mr.apemi@gmail.com>
2022-07-04 15:16:12 +0300

    Initial commit

---
+++ app/src/main/java/com/example/favoritefilmsactors/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.favoritefilmsactors
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.favoritefilmsactors.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    val binding = _binding?: throw RuntimeException("ActivityMainBinding is null")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+}
